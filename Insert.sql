cf169404837278a116586a79e049bfc45a8fd51c
VishalPhirkoj <vishalphirkoj13@gmail.com>
2021-11-25 21:51:25 +0530

    insert and select

---
+++ Insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO students (firstname, lastname, email, phone)
+VALUES
+    ('Vishal','Phirkoj','vishal@mail.com',9874563210),
+    ('Vaishnavi','Phirkoj','vaishnavi@mail.com',9874563210),
+    ('Ram','Veggalam','ram@mail.com',9874563215),
+    ('Sanju','Veggalam','sanju@mail.com',9874563215),
+	('Sai','Veggalam','sai@mail.com',9874563212),
+	('Rathan','Veggalam','rathan@mail.com',9874563218);
+
+--Output
+-- 	INSERT 0 6
+
+-- Query returned successfully in 220 msec.
+
+----------------------------------------------------------------------------------------------
+
+
+INSERT INTO department (department_name)
+VALUES
+	('Information Technology'),('Mechanical'),('Civil'),('Hotel Managment'),('Electronics and Telecomunication'),('Computer Science')
+
+--Output
+-- INSERT 0 6
+
+-- Query returned successfully in 412 msec.
---
+++ Select.sql
@@ -0,0 +1,9 @@
+Select * from students;
+
+--Output
+-- 6	"Vishal"	"Phirkoj"	"vishal@mail.com"	9874563210
+-- 7	"Vaishnavi"	"Phirkoj"	"vaishnavi@mail.com"	9874563210
+-- 8	"Ram"	"Veggalam"	"ram@mail.com"	9874563215
+-- 9	"Sanju"	"Veggalam"	"sanju@mail.com"	9874563215
+-- 10	"Sai"	"Veggalam"	"sai@mail.com"	9874563212
+-- 11	"Rathan"	"Veggalam"	"rathan@mail.com"	9874563218

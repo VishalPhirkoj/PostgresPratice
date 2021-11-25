5ecec824bd4668c639765eb4fb34581aa2f0da31
VishalPhirkoj <vishalphirkoj13@gmail.com>
2021-11-25 21:30:18 +0530

    createTable

--- CreateTable.sql
+++ CreateTable.sql
@@ -18,11 +18,27 @@ CREATE TABLE department(
    department_name VARCHAR (255) UNIQUE NOT NULL
 );
 
-Output
-CREATE TABLE
+--Output
+--CREATE TABLE
 
-Query returned successfully in 656 msec.
+--Query returned successfully in 656 msec.
 
 --------------------------------------------------------------------------------
 
 
+CREATE TABLE students_department (
+  user_id INT NOT NULL,
+  department_id INT NOT NULL,
+  grant_date TIMESTAMP,
+  PRIMARY KEY (user_id, department_id),
+  FOREIGN KEY (department_id)
+      REFERENCES department (department_id),
+  FOREIGN KEY (user_id)
+      REFERENCES students (user_id)
+);
+
+
+--Ouput
+--CREATE TABLE
+--
+--Query returned successfully in 1 secs 95 msec.
